be71ea5044ae0e7fa2f9ae97ec47f61f805833f5
marian brinza <branza.marian03@yahoo.com>
2022-11-07 21:45:32 +0200

    add util functions

--- src/util.js
+++ src/util.js
@@ -1,40 +1,55 @@
-import {nrOfPokemons, pokeApi, spritesUrl} from "./constants";
-
-export const fetchPokemons = async () => {
-  console.log('a')
-  const promises = [];
-
-  for (let i = 1; i < nrOfPokemons + 1; i++) {
-    promises.push(fetch(`${pokeApi}/${i}`).then(res => res.json()));
-  }
-
-  const results = await Promise.all(promises);
-  let pokemons = results.map(pokemon => ({
+export const fetchPokemonsInfo = (data) => {
+  const pokemons = data.map((pokemon, index) => ({
     name: pokemon.name,
-    id: pokemon.id,
-    img: pokemon.sprites.front_default,
-    type: pokemon.types.map(t => t.type.name).join(', ')
+    url: pokemon.url,
+    id: index + 1,
+    // img: `${spritesUrl}${index + 1}.png`
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index + 1}.svg`
   }));
-
-  console.log(pokemons)
   return pokemons;
 }
 
-export const getPokemonsInfo = (data) => {
-  const poke = data.map(pokemon => ({
-    name: pokemon.name,
-    id: pokemon.id,
-    img: pokemon.sprites.front_default,
-    type: pokemon.types.map(t => t.type.name).join(', ')
-  }));
-  return poke;
+export const setCssClasses = () => {
+  const pokeGrid = document.querySelector('.pokeGrid')
+  const ul = document.querySelector('.pokeGrid ul');
+  pokeGrid.classList.add('mr-150');
+  if (ul.classList.contains('center')) {
+    ul.classList.remove('center');
+    ul.classList.add('spaceBetween');
+  }
 }
 
-export const getPokemonsInfo2 = (data) => {
-  const poke = data.map((pokemon, index) => ({
-    name: pokemon.name,
-    id: index +1,
-    img: `${spritesUrl}${index+1}.png`
-  }));
-  return poke;
-}
+
+export const getHeight = (pokemonHeight) => {
+  let height;
+  if (pokemonHeight < 10) {
+    height = `0.${pokemonHeight}`
+  } else {
+    height = Number(pokemonHeight) / 10;
+  }
+  return height;
+};
+
+
+export const getPokemonDetails = (data) => {
+  const stats = {};
+  data.stats.forEach((s) => stats[s.stat.name] = s.base_stat);
+
+  let abilities = [];
+  data.abilities.forEach((a) => abilities.push(a.ability.name));
+
+  let height = getHeight(data.height);
+  const pokemonDetails = {
+    name: data.name,
+    id: `#${data.id.toString().padStart(3, '0')}`,
+    type: data.types.map(t => t.type.name),
+    img2: data.sprites.front_default,
+    img: data.sprites.other.dream_world.front_default,
+    weight: Number(data.weight) / 10,
+    height: height,
+    baseExperience: data.base_experience,
+    stats,
+    abilities
+  }
+  return pokemonDetails;
+};
